f55f8613801ce29ab5f1a9cfac09e0c00853eda4
WickedSerpent <74629174+WickedSerpent@users.noreply.github.com>
2022-03-13 20:00:36 +0100

    masterofpuppets

---
+++ klokkescript.js
@@ -0,0 +1,21 @@
+setInterval(setClock, 1000)
+
+const timeHånd = document.querySelector('[data-hour-hand]')
+const minutthånd = document.querySelector('[data-minute-hand]')
+const sekundhånd = document.querySelector('[data-second-hand]')
+
+function setClock() {
+  const currentDate = new Date()
+  const sekunder = currentDate.getSeconds() / 60
+  const minutter = (sekunder + currentDate.getMinutes()) / 60
+  const timer = (minutter + currentDate.getHours()) / 12
+  setRotation(sekundhånd, sekunder)
+  setRotation(minutthånd, minutter)
+  setRotation(timeHånd, timer)
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
